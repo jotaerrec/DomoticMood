06dc3fe8925099410926e71fb5ea1c825678bb3a
jotaerrec <javi.r49915@gmail.com>
2022-05-21 10:28:11 -0300

    Working in client

--- clientsocket/src/App.js
+++ clientsocket/src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
+import styles from "./styles.module.scss";
 
 function App() {
+  const [hamburger, setHamburger] = useState({
+    open: false,
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.Home}>
+      <div className={styles.Card}>
+        <div className={styles.topCard}>
+          <div className={styles.cardTittle}>
+            <div className={styles.circulo}></div>
+            <h1> DomoticMood </h1>
+          </div>
+          <div
+            className={styles.hamburger}
+            onClick={() => {
+              var stateH = hamburger.open;
+              setHamburger({ open: !stateH });
+            }}
+          >
+            <div
+              className={hamburger.open ? styles.changeBar1 : styles.bar1}
+            ></div>
+            <div
+              className={hamburger.open ? styles.changeBar2 : styles.bar2}
+            ></div>
+            <div
+              className={hamburger.open ? styles.changeBar3 : styles.bar3}
+            ></div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
