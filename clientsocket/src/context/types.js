3c1fc086af22efb658bceb3d6043edb1f92f3d51
jotaerrec <javi.r49915@gmail.com>
2022-11-18 01:17:11 -0300

    Change url

--- clientsocket/src/context/types.js
+++ clientsocket/src/context/types.js
@@ -3,4 +3,4 @@ export const MENU = "MENU";
 export const LOGIN = "LOGIN";
 export const ADDSCREEN = "ADDSCREEN";
 export const PROFILE = "PROFILE";
-export const URL_API = "https://35.247.216.65:8080";
+export const URL_API = "http://35.247.216.65:8080/";
